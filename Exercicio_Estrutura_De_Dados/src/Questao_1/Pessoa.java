f884ddc152a61e36b35eceae3f878c74c158c90e
Wagner Ramos Limas <50498155+guinhorl@users.noreply.github.com>
2019-05-14 20:44:46 -0300

    Exercicio_V 0.1
    Foco na foca!

---
+++ Exercicio_Estrutura_De_Dados/src/Questao_1/Pessoa.java
@@ -0,0 +1,46 @@
+package Questao_1;
+
+public class Pessoa {
+	
+	private String nome;
+	private char sexo;
+	private double altura;
+	
+	public Pessoa(String nome, char sexo, double altura) {
+		this.nome = nome;
+		this.sexo = sexo;
+		this.altura = altura;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [nome=" + nome + ", sexo=" + sexo + ", altura=" + altura + "]";
+	}
+	
+	
+
+}
